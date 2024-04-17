d3d292e5b6b0ad3dba89f79e480c74445eaf8929
Ricardo Lopez <rlopez@next-ecommerce.com>
2024-04-18 01:09:38 +0200

    AdvertsPage

--- src/pages/adverts/AdvertsPage.js
+++ src/pages/adverts/AdvertsPage.js
@@ -1,5 +1,6 @@
 import getAdverts from './service.js'
 import formatPrice from '../../helper/formatPrice.js'
+import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
@@ -57,30 +58,33 @@ const AdvertsPage = session => {
   adverts(session) */
 
   return (
-    <Row xs={1} sm={2} md={3} lg={4} className='g-4'>
-      {adverts.map(advert => (
-        <Col key={advert.id}>
-          <Card>
-            <Card.Img variant='top' src={advert.photo} />
-            <Card.Body>
-              <Card.Title>{advert.name}</Card.Title>
-              <Card.Text>
-                {`${formatPrice(advert.price)} ${advert.sale ? 'Venta' : 'Compra'}`}
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <Stack direction='horizontal' gap={2}>
-                {advert.tags.map(tag => (
-                  <Badge pill bg='primary'>
-                    {tag}
-                  </Badge>
-                ))}
-              </Stack>
-            </Card.Footer>
-          </Card>
-        </Col>
-      ))}
-    </Row>
+    <Container>
+      <Row xs={1} sm={2} md={3} lg={3} className='g-4'>
+        {adverts.map(advert => (
+          <Col key={advert.id}>
+            <Card>
+              <Card.Img variant='top' src={advert.photo} />
+              <Card.Body>
+                <Card.Title>{advert.name}</Card.Title>
+                <Card.Text className='d-flex justify-content-between align-items-center'>
+                  {formatPrice(advert.price)}{' '}
+                  <span>{advert.sale ? 'Venta' : 'Compra'}</span>
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <Stack direction='horizontal' gap={2}>
+                  {advert.tags.map(tag => (
+                    <Badge pill bg='primary'>
+                      {tag}
+                    </Badge>
+                  ))}
+                </Stack>
+              </Card.Footer>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   )
 }
 
