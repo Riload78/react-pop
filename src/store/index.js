906d53e9f72b0d4d82ff43e3abab586287d60a07
Ricardo Lopez <rlopez@next-ecommerce.com>
2024-06-06 15:49:15 +0200

    Change store redux

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,10 @@ import * as reducers from './reducers'
 
 const reducer = combineReducers(reducers)
 
-export default function configureStore() {
+export default function configureStore(preloadedState) {
   const store = createStore(
     reducer,
+    preloadedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 
